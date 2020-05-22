e14f7e252786a419ea23f3f247f604588c25905a
Wintoro <wint.design@gmail.com>
2020-05-22 11:44:17 +0700

    create a basic CRUD (create, read, update, and delete) API

---
+++ create.js
@@ -0,0 +1,28 @@
+import * as uuid from "uuid";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamoDb-lib";
+
+export const main = handler( async (event, context, callback) => {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.tableName,
+        // 'Item' contains the attributes of the item to be created
+        // - 'userId': user identities are federated through the
+        //             Cognito Identity Pool, we will use the identity id
+        //             as the user id of the authenticated user
+        // - 'noteId': a unique uuid
+        // - 'content': parsed from request body
+        // - 'attachment': parsed from request body
+        // - 'createdAt': current Unix timestamp
+        Item: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: uuid.v1(),
+            content: data.content,
+            attachment: data.attachment,
+            createAt: Date.now()
+        }
+    };
+
+    await dynamoDb.put(params);
+    return params.Item;
+});
