4dd079bc16debd23278bd0328194c6be58ef28e4
Wintoro <wint.design@gmail.com>
2020-05-28 10:23:17 +0700

    Fix TableName parameter on List and Get

--- get.js
+++ get.js
@@ -3,7 +3,7 @@ import dynamoDb from "./libs/dynamoDb-lib";
 
 export const main = handler(async (event, context) => {
     const params = {
-        TableName: process.env.TableName,
+        TableName: process.env.tableName,
         // 'Key' defines the partition key and sort key of the item to be retrieved
         // - 'userId': Identity Pool identity id of the authenticated user
         // - 'noteId': path parameter
--- list.js
+++ list.js
@@ -3,13 +3,7 @@ import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler( async(event, context) => {
     const params = {
-        TableName: process.env.TableName,
-        // 'KeyConditionExpression' defines the condition for the query
-        // - 'userId = :userId': only return items with matching 'userId'
-        //   partition key
-        // 'ExpressionAttributeValues' defines the value in the condition
-        // - ':userId': defines 'userId' to be Identity Pool identity id
-        //   of the authenticated user
+        TableName: process.env.tableName,
         KeyConditionExpression: "userId = :userId",
         ExpressionAttributeValues: {
             ":userId": event.requestContext.identity.cognitoIdentityId
